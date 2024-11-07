693385878e8225104a2ea0bae48847602919aec4
Regina Chua <19302573+rchuable@users.noreply.github.com>
2024-11-07 11:43:50 -0500

    saved files

--- app.py
+++ app.py
@@ -20,6 +20,13 @@ from io import BytesIO
 app = Flask(__name__)
 
 # FUNCTIONS ---
+
+# adjust text color to brightness
+def get_text_color(hex_color):
+    r, g, b = int(hex_color[1:3], 16), int(hex_color[3:5], 16), int(hex_color[5:7], 16)
+    brightness = (r * 299 + g * 587 + b * 114) / 1000
+    return "#FFFFFF" if brightness < 128 else "#000000"
+
 # get color palette from an image URL
 def get_color_palette(url, color_count=5):
     try:
@@ -34,7 +41,10 @@ def get_color_palette(url, color_count=5):
 
         # Get palette and hex code
         palette = color_thief.get_palette(color_count=color_count)
-        hex_palette = ['#{:02x}{:02x}{:02x}'.format(r, g, b) for r, g, b in palette]
+        hex_palette = [{'color': '#{:02x}{:02x}{:02x}'.format(r, g, b), 
+                        'text_color': get_text_color('#{:02x}{:02x}{:02x}'.format(r, g, b))}
+                        for r, g, b in palette]
+        #hex_palette = ['#{:02x}{:02x}{:02x}'.format(r, g, b) for r, g, b in palette]
 
         return hex_palette
 
